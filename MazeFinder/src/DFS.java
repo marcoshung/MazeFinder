18a8962f631357ba60ca6883b5c20135e1a6d59c
Marcos Hung <mhung927@gmail.com>
2019-11-09 21:33:53 -0800

    started DFS

---
+++ MazeFinder/src/DFS.java
@@ -0,0 +1,32 @@
+
+public class DFS {
+	public boolean DFS(Graph g) {
+		for(Node n: g.vertex.children) {
+			n.color = "white";
+			n.parent = null;
+			n.time = 0;
+		}
+		for(Node n: g.vertex.chlidren) {
+			if(n.color.equals("white")) {
+				visit(g,n);
+			}
+		}
+	}
+	
+	public void visit(Graph g, Node n) {
+		n.color = "grey";
+		g.time++;
+		n.discovery = g.time;
+		
+		for(Node adj: n.children) {
+			if(adj.color.equals("white")) {
+				adj.parent = n;
+				visit(g, adj);
+			}
+		}
+		
+		n.color = "black";
+		g.time++;
+		n.fin = g.time;
+	}
+}
