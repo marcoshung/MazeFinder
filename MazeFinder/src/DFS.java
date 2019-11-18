4328b731dbe36c46f5bbb0566ca5c86d9e43d300
Marcos Hung <mhung927@gmail.com>
2019-11-17 18:36:18 -0800

    dfs

--- MazeFinder/src/DFS.java
+++ MazeFinder/src/DFS.java
@@ -1,19 +1,24 @@
 
 public class DFS {
-	public boolean DFS(Graph g) {
+	public void DFS(Graph g) {
 		//initializes all nodes in the graph
-		for(Node n: g.getVertex().neighbors) {
+		for(int i = 0; i < g.graph.length; i++) {
+			Node n = (Node) g.graph[i].getFirst();
 			n.color = "white";
 			n.parent = null;
+			System.out.println(n.id);
 		}
+		
 		g.time = 0;
-		Node source = g.vertex;
-		visit(g, source);
+		Node source = (Node) g.graph[0].getFirst();
+		//visit(g, source);
+		/*
 		for(Node n: g.vertex.neighbors) {
 			if(n.color.equals("white")) {
 				visit(g,n);
 			}
 		}
+		*/
 	}
 	
 	public void visit(Graph g, Node n) {
